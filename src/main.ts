7ca21efa0ff55676ca5de5a04092d86289147321
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-08 00:51:16 +0200

    moved axios settings to plugins folder

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 // axios
-import axios from "axios";
-import VueAxios from "vue-axios";
+import axios from "./plugins/axios.js";
 
-createApp(App).use(router).use(VueAxios, axios).mount("#app");
+createApp(App).use(router).use(axios).mount("#app");
