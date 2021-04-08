0393055fcf4d1f5fe1212f7bfaa334460b5527d1
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-08 15:47:42 +0200

    added vue upload plugin

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 // axios
 import axios from "./plugins/axios.js";
+// upload
+import upload from "./plugins/upload.js";
 
-createApp(App).use(router).use(axios).mount("#app");
+createApp(App).use(router).use(axios).use(upload).mount("#app");
