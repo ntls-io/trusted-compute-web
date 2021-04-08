0a3b292dc5202b7db994d4f13b47b566d16a1fa9
Pi Delport <pi@registree.io>
2021-04-08 18:36:23 +0200

    style: run "yarn lint"

--- src/plugins/upload.js
+++ src/plugins/upload.js
@@ -1,16 +1,16 @@
 import { createUpload } from "@websanova/vue-upload/src/v3.js";
 import driverHttpAxios from "@websanova/vue-upload/src/drivers/http/axios.js";
 
-export default app => {
+export default (app) => {
   app.use(
     createUpload({
       plugins: {
-        http: app.axios
+        http: app.axios,
       },
       drivers: {
-        http: driverHttpAxios
+        http: driverHttpAxios,
       },
-      options: {}
+      options: {},
     })
   );
 };
