5b0c25dd5bb88c432872073ba962047e621479a2
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-13 17:31:12 +0200

    separate crypto function

---
+++ src/utils/cryptography.ts
@@ -0,0 +1,7 @@
+import CryptoJS from "crypto-js";
+
+const encrypt = (file: string, secret: string): CryptoJS.lib.CipherParams => {
+    return CryptoJS.AES.encrypt(file, secret);
+};
+
+export { encrypt };
