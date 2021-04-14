25d2a2730071048e666cfa5e4790e5fed7fb76cb
Pi Delport <pi@registree.io>
2021-04-14 11:58:22 +0200

    style: yarn lint

--- src/utils/cryptography.ts
+++ src/utils/cryptography.ts
@@ -1,7 +1,7 @@
 import CryptoJS from "crypto-js";
 
 const encrypt = (file: string, secret: string): CryptoJS.lib.CipherParams => {
-    return CryptoJS.AES.encrypt(file, secret);
+  return CryptoJS.AES.encrypt(file, secret);
 };
 
 export { encrypt };
