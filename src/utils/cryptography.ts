ea428805123f256c23ac46e8f06822d030c698be
Pi Delport <pi@registree.io>
2021-04-14 21:57:49 +0200

    feat(utils): use tweetnacl for cryptography

--- src/utils/cryptography.ts
+++ src/utils/cryptography.ts
@@ -1,7 +1,48 @@
-import CryptoJS from "crypto-js";
+/**
+ * Cryptographic helpers.
+ */
+import nacl from "tweetnacl";
+import util from "tweetnacl-util";
 
-const encrypt = (file: string, secret: string): CryptoJS.lib.CipherParams => {
-  return CryptoJS.AES.encrypt(file, secret);
-};
+/**
+ * Encrypt a Blob (File) with a new secret key.
+ *
+ * This is currently one-shot: the Blob should fit in memory.
+ * For streaming encryption, we should look at nacl-stream-js or nacl-blob.
+ *
+ * @see https://github.com/dchest/nacl-stream-js
+ * @see https://github.com/bcomnes/nacl-blob
+ */
+export async function encryptBlob(
+  plainBlob: Blob
+): Promise<{
+  cipherBlob: Blob;
+  keyBase64: string;
+  nonceBase64: string;
+}> {
+  const plaintext = new Uint8Array(await plainBlob.arrayBuffer());
+  const { ciphertext, key, nonce } = encryptSecretbox(plaintext);
+  return {
+    cipherBlob: new Blob([ciphertext.buffer], {
+      type: "application/octet-stream",
+    }),
+    keyBase64: util.encodeBase64(key),
+    nonceBase64: util.encodeBase64(nonce),
+  };
+}
 
-export { encrypt };
+/**
+ * Helper: Encrypt data using NaCl secretbox with a new secret key.
+ */
+function encryptSecretbox(
+  plaintext: Uint8Array
+): {
+  ciphertext: Uint8Array;
+  key: Uint8Array;
+  nonce: Uint8Array;
+} {
+  const nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+  const key = nacl.randomBytes(nacl.secretbox.keyLength);
+  const ciphertext = nacl.secretbox(plaintext, nonce, key);
+  return { ciphertext, key, nonce };
+}
