fc602e3620467cdf32b9a5c03c0536fe78c014ab
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-15 20:56:29 +0200

    add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import router from "./router";
 import axios from "./plugins/axios";
 import upload from "./plugins/upload";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 createApp(App).use(router).use(axios).use(upload).mount("#app");
