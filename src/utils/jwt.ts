e846d35ea1b7f9ddd68c3ab77c852ad4ab42053e
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-29 09:51:39 +0200

    jwt utils (jose)

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,1 +1,17 @@
-import { jwtDecrypt } from "jose/jwt/decrypt";
+import { jwtVerify, JWTVerifyGetKey } from "jose/jwt/verify";
+import { createRemoteJWKSet } from "jose/jwks/remote";
+import { JWTVerifyResult } from "jose/webcrypto/types";
+
+const issuer = "https://sharedeus.eus.attest.azure.net";
+const JWKS: JWTVerifyGetKey = createRemoteJWKSet(new URL(issuer));
+
+export async function verifyToken(token: string): Promise<JWTVerifyResult> {
+  const { payload, protectedHeader } = await jwtVerify(token, JWKS, {
+    issuer
+  });
+
+  console.log(payload);
+  console.log(protectedHeader);
+
+  return { payload, protectedHeader };
+}
