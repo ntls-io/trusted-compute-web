7544893503343488eecbb4b75a5bd6f1884b726d
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-29 17:18:19 +0200

    install element plugin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import axios from './plugins/axios';
-import upload from './plugins/upload';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "./plugins/axios";
+import upload from "./plugins/upload";
+import installElementPlus from "./plugins/element";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-
-createApp(App).use(router).use(store).use(axios).use(upload).mount('#app');
+const app = createApp(App);
+installElementPlus(app);
+app.use(router).use(store).use(axios).use(upload).mount("#app");
