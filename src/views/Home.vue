a5634993974de5930b89e05a4bd928d6076765d0
Jonathan Ngbonga <jongbonga@gmail.com>
2021-04-29 23:05:34 +0200

    converted Home file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,64 @@
 <template>
-  <div class="container-fluid my-3">
-    <div class="row">
-      <div class="col-sm-8">
-        <div class="card">
-          <div
-            class="card-header d-flex justify-content-between align-items-center"
-          >
-            <span class="fw-bold">JWT Token</span>
-            <span>
-              <button class="btn btn-primary btn-sm mx-1" @click="fetchJWT()">
-                Fetch
-              </button>
-              <button
-                class="btn btn-primary btn-sm mx-1"
-                @click="copy($store.state.jwtToken)"
-                :disabled="!$store.state.jwtToken"
-              >
-                Copy
-              </button>
-              <button
-                class="btn btn-primary btn-sm mx-1"
-                @click="getAttestation($store.state.jwtToken)"
-                :disabled="!$store.state.jwtToken"
-              >
-                Validate
-              </button>
-            </span>
-          </div>
-          <div class="card-body">
-            <div v-if="busy.fetchingToken" class="text-center">
-              <div class="spinner-border text-center" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
+  <el-main>
+    <el-row :gutter="10">
+      <el-col :span="16">
+        <el-card v-loading="busy.fetchingToken">
+          <template #header>
+            <div class="card-header">
+              <span>JWT Token</span>
+              <span>
+                <el-button size="small" @click="fetchJWT()"> Fetch </el-button>
+                <el-button
+                  size="small"
+                  @click="copy($store.state.jwtToken)"
+                  :disabled="!$store.state.jwtToken"
+                >
+                  Copy
+                </el-button>
+                <el-button
+                  size="small"
+                  @click="getAttestation($store.state.jwtToken)"
+                  :disabled="!$store.state.jwtToken"
+                >
+                  Validate
+                </el-button>
+              </span>
             </div>
-            <p v-else class="card-text small">
-              {{ $store.state.jwtToken }}
-            </p>
+          </template>
+          <div>
+            {{ $store.state.jwtToken }}
           </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="row">
-          <div class="col-12" v-for="(key, i) in user_attestation" :key="i">
-            <div class="card">
-              <div class="card-header">
-                <span class="fw-bold">{{ key }}</span>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-row :gutter="15">
+          <el-col v-for="(key, i) in user_attestation" :key="i">
+            <el-card>
+              <template #header>
+                <span>{{ key }}</span>
+              </template>
+              <div>
+                {{ attestation[key] }}
               </div>
-              <div class="card-body">
-                <p class="card-text small">
-                  {{ attestation[key] }}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+            </el-card>
+          </el-col>
+        </el-row>
         <div>
           <FileUpload />
         </div>
-      </div>
-    </div>
-  </div>
+      </el-col>
+    </el-row>
+  </el-main>
 </template>
 
 <style lang="scss" scoped>
-.card {
-  margin-bottom: 15px;
+.el-col {
+  margin-bottom: 10px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
 
@@ -107,7 +101,10 @@ export default defineComponent({
     async fetchJWT() {
       this.busy.fetchingToken = true;
       await this.axios
-        .get(process.env.VUE_APP_JWT_URL)
+        .get(
+          process.env.VUE_APP_JWT_URL ??
+            "https://rtc-data.registree.io/data/attest"
+        )
         .then(({ status, data }) => {
           if (status === 200) {
             this.$store.dispatch("saveToken", data);
