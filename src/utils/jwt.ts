6d7fe365af5f03358383741f46d2e0ef3960ed3e
Herman <herman@registree.io>
2021-04-29 09:51:40 +0200

    feat(jwt): add jwt verification

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,17 +1,47 @@
-import { jwtVerify, JWTVerifyGetKey } from "jose/jwt/verify";
-import { createRemoteJWKSet } from "jose/jwks/remote";
-import { JWTVerifyResult } from "jose/webcrypto/types";
+// Cannot use endpoint since it does not allow any origin
+// Source: https://sharedeus.eus.attest.azure.net/certs
+// TODO: File an issue or figure out what we are doing wrong
+import certs from "./certs.json";
+import { KEYUTIL, b64utohex, RSAKey, X509 } from "jsrsasign";
+import { KJUR } from "jsrsasign";
 
-const issuer = "https://sharedeus.eus.attest.azure.net";
-const JWKS: JWTVerifyGetKey = createRemoteJWKSet(new URL(issuer));
+const ISSUER = "https://sharedeus.eus.attest.azure.net";
 
-export async function verifyToken(token: string): Promise<JWTVerifyResult> {
-  const { payload, protectedHeader } = await jwtVerify(token, JWKS, {
-    issuer
+// Keeping the function async since we will need it once the
+// we add the network call to get the certs
+export async function verifyToken(token: string): Promise<any> {
+  const [header, payload] = token.split(".");
+
+  const headObj = KJUR.jws.JWS.readSafeJSONString(atob(header));
+
+  if (headObj == null) {
+    throw new Error("Invalid Token");
+  }
+
+  const key = getVerifyKey((headObj as { kid: string }).kid);
+
+  const isValid = KJUR.jws.JWS.verifyJWT(token, key, {
+    iss: [ISSUER],
+    alg: ["RS256"],
+    gracePeriod: 20 * 20 * 20 * 100 * 100
   });
 
-  console.log(payload);
-  console.log(protectedHeader);
-
-  return { payload, protectedHeader };
+  if (isValid) {
+    return KJUR.jws.JWS.readSafeJSONString(atob(payload));
+  } else {
+    throw new Error("Verification for JWT token failed");
+  }
 }
+
+const getVerifyKey = (kid: string) => {
+  const candidate = certs.keys.find(jwk => jwk.kid === kid);
+  if (!candidate) {
+    throw new Error("Signing Key not found for JWT token");
+  }
+
+  const x509 = new X509();
+  candidate.x5c.forEach(cert => x509.readCertHex(b64utohex(cert)));
+
+  return x509.getPublicKey() as RSAKey;
+};
+
