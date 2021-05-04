53cea4f210c9927e847851dc51f4fc782305233a
Pi Delport <pi@registree.io>
2021-05-04 02:47:43 +0200

    chore: add Jest (Vue unit-jest)
    vue add unit-jest
    vue upgrade --next unit-jest
    
    yarn remove @vue/test-utils
    yarn add --dev @testing-library/vue@next
    yarn add --dev babel-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,14 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+  },
+
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
