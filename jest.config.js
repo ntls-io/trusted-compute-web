d2cffb4aa5d64c972e217e0a56d6412f50a092c4
Pi Delport <pi@registree.io>
2021-05-04 02:48:50 +0200

    chore(jest): make Jest work with ES modules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
+// node_modules paths that should be treated as ES modules.
+const esm = ["@websanova/vue-upload"];
+
 module.exports = {
   preset: "@vue/cli-plugin-components-jest/presets/typescript-and-babel",
   transform: {
     "^.+\\.vue$": "vue-jest"
-  }
+  },
+  // Make Jest work with ES modules.
+  // See:
+  // https://jestjs.io/docs/configuration#transformignorepatterns-arraystring
+  // https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization
+  transformIgnorePatterns: [
+    `/node_modules/(?!(${esm.join("|")})/)`,
+    "\\.pnp\\.[^\\/]+$"
+  ]
 };
