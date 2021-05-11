b9be5f0863658fd486d1d3683b0f8d963a27af33
Jonathan Ngbonga <jongbonga@gmail.com>
2021-05-11 15:37:08 +0200

    form validation

--- src/components/AccessForm.vue
+++ src/components/AccessForm.vue
@@ -1,36 +1,38 @@
 <template>
-  <el-form label-width="auto" :model="form">
-    <el-form-item label="Key">
+  <el-form label-width="auto" :model="form" :rules="rules" ref="accessForm">
+    <el-form-item label="Key" prop="access_key">
       <el-input
         v-model="form.access_key"
         placeholder="Data access key"
         type="password"
-        required
+        autocomplete="off"
       ></el-input>
     </el-form-item>
-    <el-form-item label="UUID">
+    <el-form-item label="UUID" prop="uuid">
       <el-input
         v-model="form.uuid"
         placeholder="Data UUID / Identifier"
-        required
+        autocomplete="off"
       ></el-input>
     </el-form-item>
-    <el-form-item label="Hash">
+    <el-form-item label="Hash" prop="hash">
       <el-input
         v-model="form.hash"
         placeholder="SHA-256 of the binary to authorise"
-        required
+        autocomplete="off"
       ></el-input>
     </el-form-item>
     <el-form-item label="Number of uses">
       <el-input-number
-        v-model="form.nofu"
+        v-model.number="form.nofu"
         :min="1"
         :max="100"
       ></el-input-number>
     </el-form-item>
     <el-form-item style="margin-top: 15px">
-      <el-button type="primary" @click="onSubmit">Create</el-button>
+      <el-button type="primary" @click="onSubmit('accessForm')"
+        >Create</el-button
+      >
     </el-form-item>
   </el-form>
 </template>
@@ -38,6 +40,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { encryptBlob } from "@/utils/cryptography";
+import elForm from "element-plus/lib/el-form";
 
 export default defineComponent({
   data() {
@@ -47,14 +50,46 @@ export default defineComponent({
         uuid: null,
         hash: null,
         nofu: 1
+      },
+      rules: {
+        access_key: [
+          {
+            required: true,
+            message: "Access key is required",
+            trigger: "change"
+          }
+        ],
+        uuid: [
+          {
+            required: true,
+            message: "UUID is required",
+            trigger: "change"
+          }
+        ],
+        hash: [
+          {
+            required: true,
+            message: "Hash is required",
+            trigger: "change"
+          }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      const encodedForm = new TextEncoder().encode(JSON.stringify(this.form));
-      const enclavePublicKey = this.$store.getters.enclavePublicKey;
-      encryptBlob(encodedForm, enclavePublicKey);
+    onSubmit(formName: string) {
+      (this.$refs[formName] as typeof elForm).validate((valid: boolean) => {
+        if (valid) {
+          const encodedForm = new TextEncoder().encode(
+            JSON.stringify(this.form)
+          );
+          const enclavePublicKey = this.$store.getters.enclavePublicKey;
+          encryptBlob(encodedForm, enclavePublicKey);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     }
   }
 });
